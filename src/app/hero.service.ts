6837ebea362004646ad805e92429d1d1442c02b9
Picci <enrico.piccinin@gmail.com>
2023-05-02 15:40:54 +0200

    completed up to step 5

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor(private messageService: MessageService) {}
+
+  getHeroes() {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  getHero(id: number) {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+}
